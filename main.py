6347cb944fd36159dd869725fe7eb8c03d3c5977
darkfos82 <darkfos82@gmail.com>
2024-07-29 01:01:23 +0300

    add: basic templates, statics, new directories

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 from fastapi import FastAPI
 from src.api.routers import api_v1_router
+from src.api.routers.page_router import page_router
+from fastapi.staticfiles import StaticFiles
 import uvicorn
+
 
 app: FastAPI = FastAPI(
     title="API MarketService"
+)
+app.mount(
+    "/static", StaticFiles(directory="src/static"),
+    name="static"
 )
 
 app.include_router(
     api_v1_router
 )
+app.include_router(page_router)
 
 
 if __name__ == "__main__":
